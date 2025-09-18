8276dbeab39c21d462dbdc5a2d956fd381f2fb69
AustinGL <163287485+AustinGL@users.noreply.github.com>
2025-09-18 19:40:09 +0700

    Bottom bar UI Update, color and UX improvement

--- app/src/main/java/com/example/penjualan_produk_umkm/MainActivity.kt
+++ app/src/main/java/com/example/penjualan_produk_umkm/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.penjualan_produk_umkm
 
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,30 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val beranda = findViewById<ImageButton>(R.id.beranda_icon)
+        val pesanan = findViewById<ImageButton>(R.id.pesanan_icon)
+        val profil = findViewById<ImageButton>(R.id.profil_icon)
+        val pusatbantuan = findViewById<ImageButton>(R.id.pusatbantuan_icon)
+
+        val buttons = listOf(beranda, pesanan, profil, pusatbantuan)
+        beranda.isSelected = true
+
+        beranda.setOnClickListener {
+            selectButton(buttons, beranda)
+        }
+        pesanan.setOnClickListener {
+            selectButton(buttons, pesanan)
+        }
+        profil.setOnClickListener {
+            selectButton(buttons, profil)
+        }
+        pusatbantuan.setOnClickListener {
+            selectButton(buttons, pusatbantuan)
+        }
+    }
+
+    private fun selectButton(buttons: List<ImageButton>, selected: ImageButton) {
+        buttons.forEach { it.isSelected = it == selected }
     }
 }
