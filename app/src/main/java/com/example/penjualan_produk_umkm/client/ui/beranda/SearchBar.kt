a4846e4713989a4f43ea5ac51fe0a208eb58a5a9
Moreno Pradita <morenopradita013@gmail.com>
2025-09-19 00:56:59 +0700

    Part 15 - Beranda 90% SearchBar kelar

--- app/src/main/java/com/example/penjualan_produk_umkm/client/ui/beranda/SearchBar.kt
+++ app/src/main/java/com/example/penjualan_produk_umkm/client/ui/beranda/SearchBar.kt
@@ -1,57 +1,88 @@
 package com.example.penjualan_produk_umkm.client.ui.beranda
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.material3.MaterialTheme
+import com.example.penjualan_produk_umkm.client.ui.*
 import androidx.compose.material3.Text
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import  com.example.penjualan_produk_umkm.client.ui.montserrat
 
 @Preview
 @Composable
 fun SearchBarPreview() {
     // You can pass a dummy lambda since it's just a preview
     SearchBar(
-        onSearch = { query -> /* no-op */ }
-    )
+        onSearch = { query -> /* no-op */ })
 }
 
 @Composable
 fun SearchBar(
-    modifier: Modifier = Modifier,
-    onSearch: (String) -> Unit
+    modifier: Modifier = Modifier, onSearch: (String) -> Unit
 ) {
     var textState by remember { mutableStateOf(TextFieldValue("")) }
 
-    TextField(
+    OutlinedTextField(
         value = textState,
         onValueChange = { newValue ->
             textState = newValue
             onSearch(newValue.text)
         },
-        modifier = modifier
-            .fillMaxWidth(),
-        placeholder = { Text("Cari Produk...") },
+        modifier = modifier.fillMaxWidth(),
+        placeholder = {
+            Text(
+                text = "Cari Produk...",
+                color = Secondary3,
+                fontSize = 14.sp,
+                fontFamily = montserrat,
+                )
+        },
         leadingIcon = {
             Icon(
                 imageVector = Icons.Default.Search,
                 contentDescription = "Search Icon",
+                tint = Secondary2
             )
         },
         singleLine = true,
+        shape = RoundedCornerShape(16.dp),
         colors = TextFieldDefaults.colors(
-            focusedContainerColor = MaterialTheme.colorScheme.surface,
-            unfocusedContainerColor = MaterialTheme.colorScheme.surface,
-            disabledContainerColor = MaterialTheme.colorScheme.surface,
-            focusedIndicatorColor = MaterialTheme.colorScheme.primary,
-            unfocusedIndicatorColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.2f)
-        )
+            focusedContainerColor = Color.White,
+            unfocusedContainerColor = Color.Transparent,
+            disabledContainerColor = Color.Transparent,
+            focusedIndicatorColor = Secondary2,
+            unfocusedIndicatorColor = Secondary2
+        ),
+        textStyle = LocalTextStyle.current.copy(
+            fontSize = 16.sp,
+            fontFamily = montserrat,
+            color = Secondary3
+        ),
+        trailingIcon = {
+            if (textState.text.isNotEmpty()) {
+                IconButton(onClick = {
+                    textState = TextFieldValue("") //
+                    onSearch("")
+                }) {
+                    Icon(
+                        imageVector = Icons.Default.Close,
+                        contentDescription = "Clear Text",
+                        tint = Secondary2
+                    )
+                }
+            }
+        },
     )
 
 }
